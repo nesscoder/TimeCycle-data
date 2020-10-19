7914a2a032c9125d9e9d736de794b5b0f157ff12
Elan Ness-Cohn <ness.cohn.elan@gmail.com>
2020-10-19 18:56:53 -0500

    added data folder

--- Scripts/SyntheticGenerationScripts/generateCSVDataFilesForEachExperiment.R
+++ Scripts/SyntheticGenerationScripts/generateCSVDataFilesForEachExperiment.R
@@ -16,12 +16,12 @@ load("../../Data/Raw/RawData_Col.Rdata")
           timePoints <- seq(from = 0, to = lng, by = smps) #get sequence of timePoints
           dfName <- names(df.list) #get Names of Data Frame to Use
           dfTemp <- df.list[which(as.numeric(substr(dfName,nchar(dfName), nchar(dfName))) == rep)] #split by rep
-          print(c(rep, lng,smps))
+          #print(c(rep, lng,smps))
           #lapply(seq_along(dfTemp), function(x){
           lapply(seq_along(dfTemp), function(x){
             data <- dfTemp[[x]] #get the data
             runName <- paste(smps,lng,names(dfTemp)[[x]],sep = "_") #create a Name for the sample Run
-            print(runName)
+            #print(runName)
             
             #Select the correct Data to Use for the analysis
             
@@ -40,7 +40,7 @@ load("../../Data/Raw/RawData_Col.Rdata")
             }
             
             dataToUse <- data[,paste("ZT_",tpInd,"_",repInd,sep = "")]
-            print(colnames(dataToUse))
+            #print(colnames(dataToUse))
             
             colnames(dataToUse) <- paste("ZT",tpInd,"_rep",repInd,sep = "")
 
@@ -49,8 +49,8 @@ load("../../Data/Raw/RawData_Col.Rdata")
             #Add Column for Names of Samples
             dataToUse$probes <- rownames(dataToUse)
             dataToUse <- dataToUse[c(dim(dataToUse)[2],1:(dim(dataToUse)[2]-1))]
-            print(colnames(dataToUse))
-            print(dataToUse[1:5,1:5])
+            #print(colnames(dataToUse))
+            #print(dataToUse[1:5,1:5])
             write.table(dataToUse, file = paste("../../Data/Processed/",runName,".txt", sep = ""), sep = "\t", row.names = F, col.names  = T, quote=FALSE)
           })
         })
@@ -66,19 +66,19 @@ load("../../Data/Raw/RawData_Col.Rdata")
           timePoints <- seq(from = 0, to = lng, by = smps) #get sequence of timePoints
           dfName <- names(df.list) #get Names of Data Frame to Use
           dfTemp <- df.list[which(as.numeric(substr(dfName,nchar(dfName), nchar(dfName))) == rep)] #split by rep
-          print(c(rep, lng,smps))
+          #print(c(rep, lng,smps))
           #lapply(seq_along(dfTemp), function(x){
           lapply(seq_along(dfTemp), function(x){
             data <- dfTemp[[x]] #get the data
             runName <- paste(smps,lng,names(dfTemp)[[x]],sep = "_") #create a Name for the sample Run
-            print(runName)
+            #print(runName)
             
             #Select the correct Data to Use for the analysis
             tpInd <- timePoints
             
             repInd <- rep(1,length(timePoints))
             dataToUse <- data[,paste("ZT_",tpInd,"_",repInd,sep = "")]
-            print(colnames(dataToUse))
+            #print(colnames(dataToUse))
             
             #colnames(dataToUse) <- paste("ZT",tpInd,sep = "")
             colnames(dataToUse) <- tpInd
@@ -87,7 +87,7 @@ load("../../Data/Raw/RawData_Col.Rdata")
             #Add Column for Names of Samples
             dataToUse$'#' <- rownames(dataToUse)
             dataToUse <- dataToUse[c(dim(dataToUse)[2],1:(dim(dataToUse)[2]-1))]
-            print(colnames(dataToUse))
+            #print(colnames(dataToUse))
             
             write.table(dataToUse, file = paste("../../Data/sw1per/",runName,".txt", sep = ""), sep = "\t", row.names = F, col.names  = T, quote=FALSE)
           })
